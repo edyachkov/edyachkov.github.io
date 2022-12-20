89ac5408f163ec2daea08f11ced756a3c0a7207b
Евгений Дьячков <e.v.dyachkov@MacBook-Air.home>
2022-12-21 02:04:26 +0300

    asdadad

--- src/routes/Portfolio.svelte
+++ src/routes/Portfolio.svelte
@@ -1,13 +1,45 @@
-<div>
+<!--div>
     <img src="ph1.png">
     <img src="ph2.png">
     <img src="ph3.png">
+</div-->
+
+<div id="pages">
+
 </div>
+
+<script lang="ts">
+
+    import { onMount } from "svelte";
+    import { PageFlip } from 'page-flip';
+
+    onMount(async () => {
+
+        let htmlParentElement = document.getElementById("pages")
+
+        let setting = {
+            width: window.innerWidth-50, // required parameter - base page width
+            height: 450,  // required parameter - base page height
+            showCover: true, 
+            mobileScrollSupport: false
+        }
+
+        const pageFlip = new PageFlip(htmlParentElement, setting);
+
+        pageFlip.loadFromImages(['ph1.png', 'ph2.png', 'ph3.png' ]);
+
+    });
+
+</script>
 
 <style>
 
 img{
     margin-top: 20px;
+}
+
+#pages{
+    padding: 0;
 }
 
 </style>
